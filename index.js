49a44923ac91462b397202a4c50b01dd23744b04
kopala123 <anri.kopaliani.1@btu.edu.ge>
2023-12-17 15:42:44 +0400

    feat: add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 const PORT = 3001;
 
 let persons = [
@@ -26,6 +27,7 @@ let persons = [
   },
 ];
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token("body", (req, res) => {
